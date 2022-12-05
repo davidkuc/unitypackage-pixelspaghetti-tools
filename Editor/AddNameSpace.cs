bd616b6d6fcdad3906ce1ef7b28ca340756d1646
davidkuc <91789757+davidkuc@users.noreply.github.com>
2022-12-05 15:12:54 +0100

    Added script for automatically adding root namespace

---
+++ Editor/AddNameSpace.cs
@@ -0,0 +1,21 @@
+using UnityEditor;
+
+public class AddNameSpace : UnityEditor.AssetModificationProcessor
+{
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        int index = path.LastIndexOf(".");
+        if (index < 0) return;
+        string file = path.Substring(index);
+        if (file != ".cs" && file != ".js" && file != ".boo") return;
+        file = System.IO.File.ReadAllText(path);
+
+        string rootNamespace = string.IsNullOrWhiteSpace(EditorSettings.projectGenerationRootNamespace) ?
+          string.Empty : $"{EditorSettings.projectGenerationRootNamespace}.";
+        file = file.Replace("TestingProject.", rootNamespace);
+
+        System.IO.File.WriteAllText(path, file);
+        AssetDatabase.Refresh();
+    }
+}
