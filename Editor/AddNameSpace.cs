b1064cb6604125e4d5be23c5a4cfff9fb85a1458
davidkuc <91789757+davidkuc@users.noreply.github.com>
2023-02-01 19:48:54 +0100

    Updated namespace adding script

--- Editor/AddNameSpace.cs
+++ Editor/AddNameSpace.cs
@@ -1,21 +1,55 @@
+using System.IO;
 using UnityEditor;
+using UnityEngine;
 
 public class AddNameSpace : UnityEditor.AssetModificationProcessor
 {
     public static void OnWillCreateAsset(string path)
     {
+        var rootNamespace = string.IsNullOrWhiteSpace(EditorSettings.projectGenerationRootNamespace) ?
+            string.Empty : $"{EditorSettings.projectGenerationRootNamespace}";
+
         path = path.Replace(".meta", "");
         int index = path.LastIndexOf(".");
-        if (index < 0) return;
-        string file = path.Substring(index);
-        if (file != ".cs" && file != ".js" && file != ".boo") return;
-        file = System.IO.File.ReadAllText(path);
+        if (index < 0)
+            return;
 
-        string rootNamespace = string.IsNullOrWhiteSpace(EditorSettings.projectGenerationRootNamespace) ?
-          string.Empty : $"{EditorSettings.projectGenerationRootNamespace}.";
-        file = file.Replace("#NAMESPACE#", rootNamespace);
+        string file = path.Substring(index);
+        if (file != ".cs" && file != ".js" && file != ".boo")
+            return;
+
+        string formattedNamespace = GetNamespace(path, rootNamespace);
+        file = System.IO.File.ReadAllText(path);
+        if (file.Contains(formattedNamespace))
+            return;
+
+        file = file.Replace(rootNamespace, formattedNamespace);
 
         System.IO.File.WriteAllText(path, file);
         AssetDatabase.Refresh();
+    }
+
+    private static string GetNamespace(string filePath, string rootNamespace)
+    {
+        filePath = filePath.Replace("Assets/Scripts/", "/")
+            .Replace('/', '.')
+            .Replace(' '.ToString(), string.Empty);
+
+        var splitPath = filePath.Split('.');
+        string pathWithoutFileName = string.Empty;
+
+        for (int i = 1; i < splitPath.Length - 2; i++)
+        {
+            if (i == splitPath.Length - 3)
+            {
+                pathWithoutFileName += splitPath[i];
+            }
+            else
+            {
+            pathWithoutFileName += splitPath[i] + '.';
+            }
+        }
+
+        return $"{rootNamespace}.{pathWithoutFileName}";
     }
 }
