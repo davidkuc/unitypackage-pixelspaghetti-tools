377a0da91460bdbcfb025e0b9837665cac6308a2
davidkuc <91789757+davidkuc@users.noreply.github.com>
2023-02-02 17:16:01 +0100

    Updated UI_Container script

--- Runtime/Scripts/UI_Container.cs
+++ Runtime/Scripts/UI_Container.cs
@@ -1,27 +1,16 @@
-using System;
 using UnityEngine;
 
-public class UI_Container : MonoBehaviour
+public abstract class UI_Container : MonoBehaviour
 {
-    protected event Action<bool> ContainerToggled;
-
-    private GameObject container;
     private GameObject canvas;
 
-    protected GameObject Container => container;
+    protected GameObject Canvas => canvas;
 
-    protected GameObject Canvas => canvas; 
+    protected virtual void Awake() => canvas = transform.Find("Canvas").gameObject;
 
-    protected virtual void Awake()
+    public bool ToggleCanvas(bool active)
     {
-        canvas = transform.Find("canvas").gameObject;
-        container = Canvas.transform.Find("container").gameObject;
-    }
-
-    public virtual bool ToggleContainer(bool active)
-    {
-        Container.SetActive(active);
-        ContainerToggled?.Invoke(Container.activeInHierarchy);
-        return Container.activeInHierarchy;
+        canvas.SetActive(active);
+        return canvas.activeInHierarchy;
     }
 }
